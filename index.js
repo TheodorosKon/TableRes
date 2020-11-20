7301535e01694765edbfe98cd6eed6585e357515
TheodorosKon <56728761+TheodorosKon@users.noreply.github.com>
2020-11-20 17:24:46 +0200

    Page resirection
    Page redirection and acquisition of table data
    Initialization work

--- index.js
+++ index.js
@@ -1,36 +1,133 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res){
-    res.send("Hello");
+    console.log('Request for index received');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-const sqlite3 = require('sqlite3').verbose();
+app.post('/Restaurant', function (req, res) {
+  console.log("Request for Restaunt received");
+  const sqlite3 = require('sqlite3').verbose();
 
-const path = require('path');
-const dbPath = path.resolve(__dirname, 'restdatabase.db');
+  const dbPath = path.resolve(__dirname, 'restdatabase.db');
 
-let db = new sqlite3.Database(dbPath, (err) => {
-    if(err) console.log(err);
-    console.log("Connected to the database")
-});
+  let db = new sqlite3.Database(dbPath, (err) => {
+      if(err) console.log(err);
+      console.log("Connected to the database")
+  });
 
-db.serialize(() => {
-    db.all(`SELECT * FROM Restaurants`, (err, row) => {
+  let reqData = req.body.restaurant;
+
+  console.log(req.body.restaurant);
+
+  db.serialize(() => {
+    db.all(`SELECT * FROM Tables WHERE RestaurantID=`+parseInt(reqData)+` AND Reserved=0`, (err, data) => {
       if (err) {
         console.error(err.message);
       }
-      console.table(row);
+      //let h = '<h1 style="background:Green;color:whitesmoke;margin:20px solid red;">Select a table for Restaurant ' + parseInt(reqData) + '</h1>';
+      let str = data;
+      console.table(data);
+
+      res.sendFile(path.join(__dirname + "/restaurant.html", { rest: parseInt(reqData) }));
     });
   });
-  
+    
   db.close((err) => {
     if (err) {
       console.error(err.message);
     }
     console.log('Close the database connection.');
   });
+});
+
+app.post('/Reservations', function (req, res) {
+  
+  console.log("Request for Reservations received");
+  const sqlite3 = require('sqlite3').verbose();
+
+  const dbPath = path.resolve(__dirname, 'restdatabase.db');
+
+  let db = new sqlite3.Database(dbPath, (err) => {
+      if(err) console.log(err);
+      console.log("Connected to the database")
+  });
+
+  let reserve = req.body.reservations;
+
+  db.serialize(() => {
+    db.all(`SELECT * FROM Tables WHERE ID=`+parseInt(reserve), (err, data) => {
+      if (err) {
+        console.error(err.message);
+      }
+      let h = '<h1 style="background:red;color:whitesmoke;margin:20px solid red;">Waiting for response for Table ' + parseInt(reserve) + '</h1>';
+      let str = data.ID;
+      console.table(data);
+      res.send(h+str);
+    });
+  });
+    
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+});
+
+app.get('/Tables', function (req, res) {
+  console.log('Request for Tables received');
+  const sqlite3 = require('sqlite3').verbose();
+
+  const dbPath = path.resolve(__dirname, 'restdatabase.db');
+
+  let db = new sqlite3.Database(dbPath, (err) => {
+    if(err) console.log(err);
+    console.log("Connected to the database")
+  });
+
+  let reqData = req.query.rest;
+
+  db.serialize(() => {
+    db.all(`SELECT * FROM Tables WHERE RestaurantID=`+parseInt(reqData)+` AND Reserved=0`, (err, data) => {
+      if (err) {
+        console.error(err.message);
+      }
+      let str = data;
+
+      res.json(JSON.stringify(data));
+    });
+  });
+    
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }});
+});
 
 const webserver = app.listen(8080, function () {
     console.log("Node Web Server is running..")
 });
+
+/*const https = require('https');
+            https.get('https://localhost:8080?rest1=1', (resp) => {
+            let data = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
+                console.log(data);
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                console.log(JSON.parse(data).explanation);
+            });
+
+            }).on("error", (err) => {
+            console.log("Error: " + err.message);*/
