402c2ee6ebe6eb725551aa483147dbec40930dab
TheodorosKon <56728761+TheodorosKon@users.noreply.github.com>
2020-11-21 16:34:35 +0200

    User Receive Confirmation
    User receiving confirmation for table recervation.
    
    -Need to make dynamic table ID persistent over new files send by the server

--- index.js
+++ index.js
@@ -60,13 +60,14 @@ app.post('/Reservations', function (req, res) {
   console.log(req.body.tables);
   let reserve = req.body.tables;
   db.serialize(() => {
-    db.run(`UPDATE Tables SET Waiting=1 WHERE TableNumber=`+ reserve +` AND RestaurantID=1`, (err, data) => {
+    db.run(`UPDATE Tables SET Waiting=1 WHERE TableNumber=`+ reserve +` AND RestaurantID=1 AND Waiting=0`, (err, data) => {
       if (err) {
         console.error(err.message);
       }
-      let h = '<h1 style="background:red;color:whitesmoke;margin:20px solid red;">Waiting for response for Table ' + parseInt(reserve) + '</h1>';
+      let h = '<h1 style="background:red;color:whitesmoke;margin:20px solid red;">Waiting for response for Table ' + parseInt(reserve) + '</h1><script></script>';
       let str = reserve;
-      res.send(h+str);
+      //res.send(h+str);
+      res.sendFile(path.join(__dirname + "/reservations.html"));
     });
   });
     
@@ -98,6 +99,38 @@ app.get('/Tables', function (req, res) {
       }
       let str = data;
       res.send(data);
+    });
+  });
+    
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }});
+});
+
+app.get('/CheckTable', function (req, res) {
+  console.log('Request for Check received');
+  const sqlite3 = require('sqlite3').verbose();
+
+  const dbPath = path.resolve(__dirname, 'restdatabase.db');
+
+  let db = new sqlite3.Database(dbPath, (err) => {
+    if(err) console.log(err);
+    console.log("Connected to the database")
+  });
+
+  let reqData = req.query.table;
+  console.log(reqData);
+  db.serialize(() => {
+    db.all(`SELECT * FROM Tables WHERE RestaurantID=1 AND Reserved=1 AND TableNumber=`+parseInt(reqData), (err, data) => {
+      if (err) {
+        console.error(err.message);
+      }
+      let str = data;
+      console.log(data);
+      if(data.length == 1){
+        res.send('Reserved');
+      }
     });
   });
     
